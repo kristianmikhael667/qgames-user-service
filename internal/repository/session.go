476cc57b5d74d162f42e4bbc1d8e16171cbebf70
kristianmikhael667 <kristianmikhael667@gmail.com>
2023-10-31 20:55:03 +0700

    user

--- internal/repository/session.go
+++ internal/repository/session.go
@@ -162,8 +162,10 @@ func (r *session) LogoutSession(ctx context.Context, phone string, device *dto.D
 	var foundDeviceID string
 
 	fmt.Println("apa ", foundDeviceID)
-	if err := r.Db.WithContext(ctx).Model(&model.Session{}).Where("user_id = ? & device_id = ?", users.UidUser, foundDeviceID).First(&sessions).Error; err != nil {
-		return "Device ID not found in session", 404, err
+	if foundDeviceID != "" {
+		if err := r.Db.WithContext(ctx).Model(&model.Session{}).Where("device_id = ?", foundDeviceID).First(&sessions).Error; err != nil {
+			return "Device ID not found in session", 404, err
+		}
 	}
 
 	if len(deviceIDSlice) >= intValue {
